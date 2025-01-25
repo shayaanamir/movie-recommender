05a37ecf40b8a1b22e624763e62971e43cefa542
Shayaan Amir <67624423+PlatinumBeing@users.noreply.github.com>
2025-01-25 09:26:20 +0530

    added UI elements via streamlit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-A website that recommends movies based on your favourites
+version https://git-lfs.github.com/spec/v1
+oid sha256:b9d523c408415802e590ab2a5536d1436a405eff3f5eb346c7e02a33d7ddd0e9
+size 58
--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-import numpy as np
-import pandas as pd
-from flask import Flask, render_template, request
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import json
-import bs4 as bs
-import urllib.request
-import pickle
-import requests
-
-data = pd.read_csv('final_data.csv')
-cv = CountVectorizer()
-vector = cv.fit_transform(data['comb'])
-similarity = cosine_similarity(vector)
-
-def recommendMovies(movie):
-    movie = movie.lower()
-    if movie not in data['movie_title'].unique():
-        print("Movie not found!")
-    else:
-        index = data[data['movie_title'] == movie].index[0]
-        distances = sorted(list(enumerate(similarity[index])),reverse=True,key = lambda x: x[1])
-        for i in distances[1:11]:
-            print(data.iloc[i[0]]['movie_title'])
-
-
-recommendMovies('the avengers')
+version https://git-lfs.github.com/spec/v1
+oid sha256:5d8fffcb03c9f110b2a10ee77659d55bd56e4fac1b26471d7a1aba7d72a43556
+size 2063
